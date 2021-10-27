1c91f4fae9c06f804e2981ca23e6932340b2964f
Pierre Schaus <pschaus@gmail.com>
2021-10-27 11:51:42 +0200

    cases age-groups

--- download.py
+++ download.py
@@ -71,6 +71,15 @@ def mortality():
     df.DATE = pd.to_datetime(df.DATE)
     df.to_csv('static/csv/be-covid-mortality.csv',index=True)
 
+def case_age_sex():
+    url="https://epistat.sciensano.be/Data/COVID19BE_CASES_AGESEX.csv"
+    s=requests.get(url).content
+    #df = pd.read_csv(io.StringIO(s.decode('latin-1')), keep_default_na=False)
+    df = pd.read_csv(io.StringIO(s.decode('utf8')))  # last line is NaN
+    df.DATE = pd.to_datetime(df.DATE)
+    df.to_csv('static/csv/be-covid-case-age-sex.csv',index=True)
+
 
 cases_hospi()
 mortality()
+case_age_sex()
